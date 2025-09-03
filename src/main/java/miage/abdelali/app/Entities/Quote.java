fbabbb36fbd6674a0f9ddf0b2fc7244775673534
RhofirAbdelali <abdo.rhofir@gmail.com>
2025-09-04 00:14:17 +0200

    clean test class

--- src/main/java/miage/abdelali/app/Entities/Quote.java
+++
@@ -1,9 +0,0 @@
-package miage.abdelali.app.Entities;
-
-import jakarta.persistence.*;
-
-@Entity
-public class Quote {
-  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
-}
--- src/main/java/miage/abdelali/app/Repositories/QuoteRepository.java
+++
@@ -1,6 +0,0 @@
-package miage.abdelali.app.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import miage.abdelali.app.Entities.Quote;
-
-public interface QuoteRepository extends JpaRepository<Quote, Long> {}
