842b8a766f9a39f55704f6ccb8e4a363fc0cb2af
RhofirAbdelali <abdo.rhofir@gmail.com>
2025-09-04 06:38:47 +0200

    ajout logique métier Product

---
+++ src/main/java/miage/abdelali/app/Services/ProductService.java
@@ -0,0 +1,12 @@
+package miage.abdelali.app.Services;
+
+import java.util.List;
+import miage.abdelali.app.Entities.Product;
+
+public interface ProductService {
+	boolean existsByCode(String code);
+
+	Product create(String code, String label);
+
+	List<Product> findAll();
+}
